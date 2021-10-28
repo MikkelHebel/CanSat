5e2dd7f9e622837a9c85829d3628180c3d0980dd
Mikkel Hebel <mikkelhebelv2@gmail.com>
2021-10-28 08:50:46 +0200

    Updated a few things

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
+const salary = require("./routes/salary.js")
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
---
+++ routes/salary.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  }
+});
+
+// Route /all
+router.get("/all", async (req, res) => {
+  try {
+    // Wait for DB connection
+    const client = await pool.connect();
+
+    // Run query
+    const result = await client.query("SELECT * FROM salaries");
+
+    // Respond with DB results as json
+    if (result) res.json({salaries: result.rows});else res.json({salaries: null});
+
+    // Release connection
+    client.release();
+  } catch (err) {
+      // Report errors
+      console.error(err);
+      res.send("Error " + err);
+  }
+});
+
+module.exports = router;
