83a86ddeb8a87838764de01305189bba9edad55b
Mikkel Hebel <mikkelhebelv2@gmail.com>
2021-11-11 09:03:44 +0100

    Updated index

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 const data = require("./routes/data.js")
+const datahandler = require("./routes/datahandler.js")
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .use("/",data)
+  .use("/data",datahandler)
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
