e01b0d57991f1968d22d1e25479ceb93880e2801
Mikkel Hebel <mikkelhebelv2@gmail.com>
2021-11-11 09:41:04 +0100

    Trying to get the chart to work

--- public/scripts/config.js
+++ public/scripts/config.js
@@ -1,18 +1,29 @@
 const labels = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
+  '00:05',
+  '00:10',
+  '00:15',
+  '00:20',
+  '00:25',
+  '00:30',
 ];
 const data = {
   labels: labels,
   datasets: [{
-    label: 'My First dataset',
+    label: 'Temperature',
     backgroundColor: 'rgb(255, 99, 132)',
     borderColor: 'rgb(255, 99, 132)',
-    data: [0, 10, 5, 2, 20, 30, 45],
+    const client = await pool.connect();
+
+    client
+        // Send query to database
+        .query("SELECT * FROM temperature")
+
+        // Handle results
+        .then((result) => {
+            const results = { data: result ? result.rows : null };
+            res.send(results);
+        })
+    //data: [0, 10, 5, 2, 20, 30, 45],
   }]
 };
 
