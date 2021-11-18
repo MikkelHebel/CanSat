7bfb99836cd2d9727450c5b3e3b87f73af7e7e20
Mikkel Hebel <mikkelhebelv2@gmail.com>
2021-11-18 11:41:25 +0100

    I need a life

--- routes/datahandler.js
+++ routes/datahandler.js
@@ -47,7 +47,7 @@ router.get("/insert", async (req, res) => {
 
     // Prepare query for database
     const query = {
-        text: "INSERT INTO data(Temperature,Humidity,Pressure,Altitude) VALUES (20.1,1,1,1);",
+        text: "INSERT INTO data(Temperature,Humidity,Pressure,Altitude) VALUES (20.1,1,5,1000);",
         values: []
     };
     console.log(query);
