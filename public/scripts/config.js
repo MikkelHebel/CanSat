328ba8ef976f005dd01607f9822267427431e39d
Mikkel Hebel <mikkelhebelv2@gmail.com>
2021-11-23 13:54:44 +0100

    Data printing didn't work, trying again

--- public/scripts/config.js
+++ public/scripts/config.js
@@ -52,7 +52,7 @@ for (let x = start; x <= end; x += 1000) {
 function fetchData(x1, x2) {
   console.log("fetchData has been called!");
   const step = Math.max(1, Math.round((x2 - x1) / 100000));
-  const data = fetch('/data/get');
+  const data = fetch('/data/get').then(response => response.json());
   console.log("Printed data:");
   console.log(data);
   //const data = [];
