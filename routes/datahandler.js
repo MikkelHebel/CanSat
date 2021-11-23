7f90eb7472520d4965521d40f4b588c0c5b0b67d
Mikkel Hebel <mikkelhebelv2@gmail.com>
2021-11-23 08:42:06 +0100

    no work

--- routes/datahandler.js
+++ routes/datahandler.js
@@ -47,7 +47,7 @@ router.get("/insert", async (req, res) => {
 
     // Prepare query for database
     const query = {
-        text: "INSERT INTO data(Temperature,Humidity,Pressure,Altitude) VALUES (?, 1, 5, 1000);",
+        text: "INSERT INTO data(Temperature,Humidity,Pressure,Altitude) VALUES (25, 1, 5, 1000);",
         values: ['temperature', 'humidity', 'pressure', 'altitude']
     };
     console.log("Temperature");
