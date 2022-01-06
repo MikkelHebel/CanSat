0935199c7ac60c2c99737dc20e0468dd0a88f985
Mikkel Hebel <mikkelhebelv2@gmail.com>
2022-01-06 11:38:39 +0100

    Trying to see if it works

--- public/scripts/config.js
+++ public/scripts/config.js
@@ -39,6 +39,7 @@ const selectedChart = document.getElementById('selectChart');
 selectedChart.addEventListener('change', choosenChart);
 function choosenChart(){
   console.log(selectedChart.value);
+  myChart.data.datasets[0].data = selectChart.value
 }
 
 const labels = [
