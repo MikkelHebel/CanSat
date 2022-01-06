eaf895ef3e279a20e333d6246deb855943d32e77
Mikkel Hebel <mikkelhebelv2@gmail.com>
2022-01-06 09:29:09 +0100

    Made a typo

--- public/scripts/config.js
+++ public/scripts/config.js
@@ -2,6 +2,8 @@ const config = {
   type: 'line',
   data: data,
 };
+console.log("Config loaded:");
+console.log(config);
 
 const labels = [
   '00:05',
@@ -11,6 +13,8 @@ const labels = [
   '00:25',
   '00:30',
 ];
+console.log("Labels loaded:");
+console.log(labels);
 
 const data = {
   labels: labels,
@@ -22,6 +26,8 @@ const data = {
     tension: 0.1
   }]
 };
+console.log("Data loaded:");
+console.log(data);
 
 const chart = new Chart(
   document.getElementById('chart'),
